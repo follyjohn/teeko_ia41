497bab91216f903e7b34b82145931b91365b14da
John FOLLY <follyjohn@outlook.fr>
2021-11-26 17:26:40 +0100

    found how to draw circle on board

--- src/teeko/view/game.py
+++ src/teeko/view/game.py
@@ -2,14 +2,17 @@ import pygame
 
 pygame.init()
 
-ecran = pygame.display.set_mode((700, 700))
+ecran = pygame.display.set_mode((720, 740))
 image = pygame.image.load("src/teeko/view/teeko_board.png").convert_alpha()
-
+image = pygame.transform.scale(image, (720, 720))
+pygame.draw.circle(image, (14, 45, 42), (127, 127), 35)
+pygame.draw.circle(image, (14, 45, 42), (242, 127), 35)
+pygame.draw.circle(image, (14, 45, 42), (242, 242), 35)
 continuer = True
 
 while continuer:
     ecran.fill((255, 255, 255))
-    ecran.blit(image, (0, 50))
+    ecran.blit(image, (0, 20))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             continuer = False
