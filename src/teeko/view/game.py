71da12c3dd76d54b4de30d8cc41cda143a90cfff
John FOLLY <follyjohn@outlook.fr>
2021-11-29 10:51:43 +0100

    update board asset

--- src/teeko/view/game.py
+++ src/teeko/view/game.py
@@ -9,11 +9,13 @@ from teeko.models.teeko_piece import TeekoPieceEnum
 board = Board()
 
 
+
+
 pygame.init()
 pygame.display.set_caption("Teeko")
-ecran = pygame.display.set_mode((720, 740))
+ecran = pygame.display.set_mode((720, 720))
 
-image = pygame.image.load("./teeko/view/teeko_board.png").convert_alpha()
+image = pygame.image.load("./src/teeko/view/1x/Artboard 1.png").convert_alpha()
 image = pygame.transform.scale(image, (720, 720))
 
 # pygame.draw.circle(image, (0, 0, 0), (127, 127), 35)
@@ -27,19 +29,17 @@ image = pygame.transform.scale(image, (720, 720))
 # pygame.draw.circle(image, (255,0,0), (357, 473), 35)
 
 positions = []
-x = 127
-y = 147
+x = 122
+y = 121
 for i in range(5):
     positions.append([])
     for j in range(5):
         positions[i].append((x, y, None))
         if board.get_piece_at_coordinate(Coordinate(i, j)) == TeekoPieceEnum.BLACK_PIECE:
             pygame.draw.circle(image, (255, 255, 255), (x, y), 35)
-        x += 115
-    if i == 1:
-        y += 2
-    x = 127
-    y += 115
+        x += 119.5
+    x = 121
+    y += 119.7
 
 print(positions)
 
@@ -55,6 +55,7 @@ def get_square_under_mouse(board):  # gest mouse position #TODO
                 for j in range(5):
                     if x <= board[i][j][0]+30 and x >= board[i][j][0]-30 and y <= board[i][j][1]+30 and y >= board[i][j][1]-30:
                         print(board[i][j])
+                        
                         return board[i][j]
     except IndexError:
         pass
@@ -73,7 +74,8 @@ def update(event_list, board):
                             for j in range(5):
                                 if x <= board[i][j][0]+30 and x >= board[i][j][0]-30 and y <= board[i][j][1]+30 and y >= board[i][j][1]-30:
                                     t,m,_ = board[i][j]
-                                    pygame.draw.circle(image, (255, 0, 0), (t,m-20), 40)
+                                    board[i][j] = (board[i][j][0], board[i][j][1], TeekoPieceEnum.BLACK_PIECE)
+                                    pygame.draw.circle(image, (255, 0, 0), (t,m), 40)
                                     
                 except IndexError:
                     pass
@@ -85,20 +87,41 @@ continuer = True
       if circle is empty: add nez piece and drow it
       else show arrow to move piece
 """
-
+clock = pygame.time.Clock()
+selected_piece = None
+drop_pos = None
 while continuer:
     ecran.fill((255, 255, 255))
-    ecran.blit(image, (0, 20))
+    ecran.blit(image, (0, 0))
     event_list = pygame.event.get()
-    for event in event_list:
-        if event.type == pygame.QUIT:
-            continuer = False
+    
     
     x, y, piece = get_square_under_mouse(positions)
-    update(event_list, positions)
     if x != None:
         pygame.draw.circle(ecran, (255, 0, 0), (x, y), 40, 2)
     pygame.display.flip()
+
+    if piece is None:
+        update(event_list, positions)
+    else: 
+        selected_piece = piece, x, y
+
+    for event in event_list:
+        if event.type == pygame.QUIT:
+            continuer = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            if drop_pos:
+                piece, old_x, old_y = selected_piece
+                board[old_y][old_x] = (board[old_y][old_x][0], board[old_y][old_x][1], None)
+                new_x, new_y = drop_pos
+                board[new_y][new_x] = (board[new_y][new_x][0], board[new_y][new_x][1], TeekoPieceEnum.BLACK_PIECE)
+            selected_piece = None
+            drop_pos = None
+
+
+    
+    
+    
 
 
 pygame.quit()
